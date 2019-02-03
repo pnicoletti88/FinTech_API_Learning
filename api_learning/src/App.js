242b95bf3d12ffefbddd99d17fc25584cdec2401
Phil <pnicolet@uwo.ca>
2019-02-03 18:39:44 -0500

    FrameWork Built

--- api_learning/src/App.js
+++ api_learning/src/App.js
@@ -1,25 +1,54 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      Name: null,
+      Price: null
+    };
+  }
+
+  StockIn(event, ticker){
+    event.preventDefault();
+    let StockName=null, Price=null;
+
+    // @matt add the API stuff here!
+    //set the variables StockName and Price equal to the data
+
+
+    this.setState({
+      Name: StockName,
+      Price: Price
+    });
+
+
+  }
+
   render() {
+    let stockData = "";
+    if(this.state.Name !== null){
+      stockData = (
+        <div>
+          Name: {this.props.Name}
+          <br/>
+          Price: {this.props.Price}
+        </div>
+      );
+    }
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <form onSubmit={(e, input = document.getElementById("TickerBox").value) => { this.StockIn(e, input) }}>
+          <label>
+            Enter the Ticker of A Stock: &nbsp;
+            <input type="text" id="TickerBox" />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+      <br/><br/>
+        {stockData}
       </div>
     );
   }
