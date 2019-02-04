889d54127727b2bd21e1086e4e2981f109014dc0
mkasperavicius <mkasper2@uwo.ca>
2019-02-03 22:21:41 -0500

    V1

--- api_learning/src/App.js
+++ api_learning/src/App.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
+import { IEXClient } from 'iex-api'
+import * as _fetch from 'isomorphic-fetch'
+const iex = new IEXClient(_fetch);
+
+
+
 
 class App extends Component {
+
   constructor(props){
     super(props);
     this.state = {
@@ -9,13 +16,76 @@ class App extends Component {
       Price: null
     };
   }
+  /*static getPrice(ticker) {
+    return iex.stockPrice(ticker.toString()).then(res => {
+      return res;
+    })
+  }*/
 
   StockIn(event, ticker){
     event.preventDefault();
     let StockName=null, Price=null;
+    alert(ticker);
+    //var promise1 = new Promise(function(resolve, reject){resolve(App.getPrice(ticker))});
+    //promise1.then((Price) => {return Price;});
 
     // @matt add the API stuff here!
     //set the variables StockName and Price equal to the data
+
+
+    //Price = iex.stockQuote(`${ticker}`,false).latestPrice;
+    var data = JSON.parse(iex.stockQuote(ticker.toString(),false));
+    alert()
+    StockName = data.companyName;
+    //Price = data.latestPrice;
+    //StockName = iex.stockQuote(ticker.toString(),false).companyName;
+    Price = iex.stockPrice(ticker.toString());
+
+
+    //iex.stockQuote(ticker.toString(),false).then(json, body)=>{return }
+
+    //StockName = "Bob";
+    //App.getPrice(ticker);
+
+
+
+    var request = new XMLHttpRequest();
+
+    request.open('GET', `https://api.iextrading.com/1.0/stock/${ticker}/quote`,false);
+    request.onload = function(){
+      var data = JSON.parse(this.response);
+      StockName = data.companyName.toString();
+      Price = data.latestPrice.toString();
+      //data.forEach()
+    };
+
+    //request.send();
+    //iex.request('https://api.iextrading.com/1.0/stock/{aapl}/book').then()
+    //JSON.parse(body);
+    /*iex.request(`https://api.iextrading.com/1.0/stock/${ticker}/book`).then((json, body) => {
+      new Quote({
+        symbol: symbol,
+        date: new Date(JSON.quote.latestUpdate),
+        source: "IEX",
+        price: {
+          last: json.quote.latestPrice,
+          open: json.quote.open,
+          high: json.quote.high,
+          low: json.quote.low,
+          close: json.quote.close,
+          volume: json.quote.latestVolume
+        },
+        dom: {
+          bids: json.bids,
+          asks: json.asks
+        },
+        original: body
+      });
+    })*/
+
+    //Price = iex.(ticker.toString());
+    //Price = json.quote.latestPrice;
+
 
 
     this.setState({
@@ -28,15 +98,16 @@ class App extends Component {
 
   render() {
     let stockData = "";
-    if(this.state.Name !== null){
+    if (this.state.Name !== null) {
       stockData = (
-        <div>
-          Name: {this.props.Name}
-          <br/>
-          Price: {this.props.Price}
-        </div>
+          <div>
+            Name: {this.state.Name}
+            <br/>
+            Price: {this.state.Price}
+          </div>
       );
     }
+
 
     return (
       <div className="App">
@@ -52,6 +123,7 @@ class App extends Component {
       </div>
     );
   }
+
 }
 
 export default App;
